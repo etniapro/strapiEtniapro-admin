d7c06f4bbb777fbcba8760789329f32d972eff98
Juan_Lorza <lorza112@gmail.com>
2023-09-25 20:14:17 -0500

    first commit main

---
+++ etniapro-admin/types/generated/components.d.ts
@@ -0,0 +1,33 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface ObjetFeatureFeatures extends Schema.Component {
+  collectionName: 'components_objet_feature_features';
+  info: {
+    displayName: 'features';
+    icon: 'bulletList';
+  };
+  attributes: {
+    feacture: Attribute.String;
+  };
+}
+
+export interface ObjetSpecificationSpecifications extends Schema.Component {
+  collectionName: 'components_objet_specification_specifications';
+  info: {
+    displayName: 'specifications';
+    icon: 'layer';
+  };
+  attributes: {
+    name: Attribute.String;
+    values: Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Shared {
+    export interface Components {
+      'objet-feature.features': ObjetFeatureFeatures;
+      'objet-specification.specifications': ObjetSpecificationSpecifications;
+    }
+  }
+}
